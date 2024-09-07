d9bc195019a9f1297493dcde8673cc73e705fe9e
LaiMartino96 <laimartino96@gmail.com>
2024-09-06 22:05:15 -0300

    test

---
+++ TP-CAI-2024-G5/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_CAI_2024_G5
+{
+    internal class Class1
+    {
+    }
+}
