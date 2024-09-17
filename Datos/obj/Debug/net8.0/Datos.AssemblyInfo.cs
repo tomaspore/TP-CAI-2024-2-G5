e4e5e4dd84a3c06abc093d14a7834d1c851bee27
brandansantiago <brandansantiago1998@gmail.com>
2024-09-16 21:26:48 -0300

    Commit git ignore
    Prueba git ignore

--- Datos/obj/Debug/net8.0/Datos.AssemblyInfo.cs
+++ Datos/obj/Debug/net8.0/Datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70227aed059ff26dd241c418bb58f758b2b2f56d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f46e0c0aaf7ed113db5a00fae0fb216aea9db812")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
