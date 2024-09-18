9950528fdda3f7c3dd3836d23f0f90411bbbe006
tomaspore <tomasalejoalberio@gmail.com>
2024-09-18 00:22:19 -0300

    Template del profe
    Migre todo al file que dieron los profes

--- Datos/Class1.cs
+++ Datos/Class1.cs
@@ -1,7 +1,20 @@
+<<<<<<< Updated upstream
 ﻿namespace Datos
 {
     public class Class1
     {
 
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Class1
+    {
+>>>>>>> Stashed changes
     }
 }
